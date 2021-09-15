6844c1e6d734fab83b6a27bbb32b50585f231b8b
aliozzkan <aliozzkan@gmail.com>
2021-09-15 09:00:59 +0300

    camera

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    var video = document.getElementById("video");
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function (stream) {
+          video.srcObject = stream;
+          video.play();
+        });
+    }
+  }, []);
+
+  function takePhoto() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var video = document.getElementById("video");
+
+    context.drawImage(video, 0, 0, 640, 480);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="video" width="640" height="480" autoplay />
+      <button id="snap" onClick={takePhoto}>
+        Snap Photo
+      </button>
+      <canvas id="canvas" width="640" height="480" />
     </div>
   );
 }
